daa244c0ac52515a3db10d0465c7136c0bb06566
Svetoslav Krasimirov Petkov <98747798+PetkovSvetoslav@users.noreply.github.com>
2023-01-04 16:29:31 +0200

    Add files via upload

---
+++ EXAMS Solutions/Java DB Advanced Exam - 28 Jul 2019/src/main/java/softuni/exam/domain/entities/Team.java
@@ -0,0 +1,54 @@
+package softuni.exam.domain.entities;
+
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.List;
+
+@Entity
+@Table(name = "teams")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String name;
+    @ManyToOne
+    private Picture picture;
+    @OneToMany(mappedBy = "team", targetEntity = Player.class)
+    private List<Player> players;
+
+    public Team(){}
+
+    @OneToMany(mappedBy = "team", targetEntity = Player.class)
+    public List<Player> getPlayers() {
+        return Collections.unmodifiableList(this.players);
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    @Column(nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @ManyToOne
+    public Picture getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Picture picture) {
+        this.picture = picture;
+    }
+}
